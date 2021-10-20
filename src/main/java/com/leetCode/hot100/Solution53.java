a2ac47b222e63c356dd354e23c6ff855caff3017
温浩然 <18240443265@163.com>
2021-10-20 18:25:48 +0800

    leetcode 53

--- src/main/java/com/leetCode/hot100/Solution53.java
+++ src/main/java/com/leetCode/hot100/Solution53.java
@@ -1,7 +1,20 @@
 package com.leetCode.hot100;
 
 public class Solution53 {
+    public static void main(String[] args) {
+        Solution53 s = new Solution53();
+        //int[] n = {-1, -2, 3, 4, 5, -6, -7, 8, 9};
+        int[] n = {-1, -2};
+        System.out.println(s.maxSubArray(n));
+    }
     public int maxSubArray(int[] nums) {
-
+        int res = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int num:nums){
+            if(sum>0){sum+=num;}
+            else{sum=num;}
+            res=Math.max(res,sum);
+        }
+        return res;
     }
 }
