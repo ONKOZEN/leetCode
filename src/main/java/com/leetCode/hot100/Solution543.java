91983d453ab0a1de3caedba2c7e7cc55ae8e9a08
温浩然 <18240443265@163.com>
2021-10-24 22:39:19 +0800

    leetcode 543

--- src/main/java/com/leetCode/hot100/Solution543.java
+++ src/main/java/com/leetCode/hot100/Solution543.java
@@ -1,5 +1,32 @@
 package com.leetCode.hot100;
 
 public class Solution543 {
+    int max = 0;
+    public static void main(String[] args) {
+        TreeNode t = new TreeNode(1);
+        t.left = new TreeNode(2);
+        t.right = new TreeNode(3);
+        t.left.left = new TreeNode(4);
+        t.left.left.right = new TreeNode(5);
+        System.out.println(new Solution543().dfs(t));
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = root.left == null ? 0 : dfs(root.left);
+        int right = root.right == null ? 0 : dfs(root.right);
+        max = Math.max(max,left+right);
+        return Math.max(left, right) + 1;
+    }
 
 }
