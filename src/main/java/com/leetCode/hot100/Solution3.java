d0a33402fed58483d91dbe25b2e045294d98e830
温浩然 <18240443265@163.com>
2021-11-05 15:52:59 +0800

    leetcode 3

--- src/main/java/com/leetCode/hot100/Solution3.java
+++ src/main/java/com/leetCode/hot100/Solution3.java
@@ -18,20 +18,14 @@ public class Solution3 {
         }
         Map<Character, Integer> map = new HashMap<>();
         int max = 0;
-        int me = 0;
-        int ms = 0;
         for (int start = 0, end = 0; end < s.length(); end++) {
             if (map.containsKey(s.charAt(end))) {
                 start = Math.max(start, map.get(s.charAt(end)) + 1);
             }
-            if(end-start+1>max){
-                me = end;
-                ms = start;
-            }
-            max = Math.max(max,end-start+1);
-            map.put(s.charAt(end),end);
+            max = Math.max(max, end - start + 1);
+            map.put(s.charAt(end), end);
         }
-        System.out.println(s.substring(ms,me+1));
         return max;
     }
+
 }
