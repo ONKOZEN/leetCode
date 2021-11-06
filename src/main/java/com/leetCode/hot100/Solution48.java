8d0fae157d02f25ea8bf96f3c02143f9a5e5958a
温浩然 <18240443265@163.com>
2021-11-06 16:16:00 +0800

    solution48

---
+++ src/main/java/com/leetCode/hot100/Solution48.java
@@ -0,0 +1,21 @@
+package com.leetCode.hot100;
+
+public class Solution48 {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        int n = matrix[0].length;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
